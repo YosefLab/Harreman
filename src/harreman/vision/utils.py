3bdab83db62a838701ac99363af4f06b50453be6
Oier Etxezarreta <oier.etxezarreta@weizmann.ac.il>
2026-03-30 17:28:03 +0300

    _get_mean_var function import modified in signature.py and utils.py file created

---
+++ src/harreman/vision/utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+from scipy import sparse
+
+def _get_mean_var(X, axis=0):
+    if sparse.issparse(X):
+        mean = np.array(X.mean(axis=axis)).ravel()
+        var = np.array(X.power(2).mean(axis=axis)).ravel() - mean**2
+    else:
+        mean = np.mean(X, axis=axis)
+        var = np.var(X, axis=axis, ddof=1)
+    return mean, var
